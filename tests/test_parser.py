68e2e9ac32d5d8869c99d3000a5700da05b5db31
blacksmokezip <hanzomainer1488@gmail.com>
2023-07-24 17:53:10 +1000

    Fix

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,7 @@
 from gendiff.parser import parse
 
 url = 'https://jsonplaceholder.typicode.com/todos/1'
-
+path = './tests/fixtures/before.json'
 
 def test_open_from_url():
     result = parse(url)
@@ -10,4 +10,25 @@ def test_open_from_url():
         "id": 1,
         "title": "delectus aut autem",
         "completed": False
+    }
+
+
+def test_open_file():
+    result = parse(path)
+    assert result == {
+      "common": {
+        "setting1": "Value 1",
+        "setting2": "200",
+        "setting3": True,
+        "setting6": {
+          "key": "value"
+        }
+      },
+      "group1": {
+        "baz": "bas",
+        "foo": "bar"
+      },
+      "group2": {
+        "abc": "12345"
+      }
     }
